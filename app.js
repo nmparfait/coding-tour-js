d94e6447793039742c6d6c162c35a304b22b0b09
nmparfait <mbamu.developer@gmail.com>
2021-04-05 11:44:04 -0400

    scrolling event added dynamically

--- app.js
+++ app.js
@@ -47,3 +47,32 @@ window.addEventListener('scroll', function () {
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    //prevent default
+    e.preventDefault();
+
+    //navigate to specific
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
